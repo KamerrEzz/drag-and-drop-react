70353679d15097a271eafb68385b5ee765bda44e
Kamerr Ezz <kamerr_oficial@hotmail.com>
2024-02-06 15:56:10 -0600

    docs: :memo: add comments
    #29

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// * Esto tiene que estart
+// ! Tener cuidado por que esto se tiene que quitar
+// ? Vamos a cambair esto?
+// // proximo a eliminar
+// TODO: agregar validacion
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
